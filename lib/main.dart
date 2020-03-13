84627f9db8e68963f601c9f2eaff3c4e1efc410b
danielleajoc <dajoc@crc.pshs.edu.ph>
2020-03-13 18:31:11 +0800

    Colors and Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,24 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text("my first app"),
       centerTitle: true,
+      backgroundColor: Colors.red,
     ),
     body: Center(
-      child: Text('hello ninjassss'),
+      child: Text(
+        'hello ninjassss',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey,
+            fontFamily: 'Nubolts',
+          )
+        ),
     ),
     floatingActionButton: FloatingActionButton(
+      onPressed: (){},
       child: Text('click'),
+      backgroundColor: Colors.red,
     ),
   )
 ));
